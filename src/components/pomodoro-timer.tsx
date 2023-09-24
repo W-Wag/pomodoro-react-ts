97932d72d206d169f2f5dc985d2ddcec28bffc79
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-09-24 13:40:40 -0300

    Aula criando botão de resting

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -14,15 +14,32 @@ export function PomodoroTimer(props: Props) {
   const [mainTime, setMainTime] = useState(props.pomodoroTime);
   const [timeCounting, setTimeCounting] = useState(false);
   const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
 
   const configureWork = () => {
     setTimeCounting(true);
     setWorking(true);
+    setResting(false);
+    setMainTime(props.pomodoroTime);
+  };
+  const configureRest = (long: boolean) => {
+    setTimeCounting(true);
+    setResting(true);
+    setWorking(false);
+
+    if (long) {
+      setMainTime(props.longRestTime);
+    } else {
+      setMainTime(props.shortRestTime);
+    }
   };
 
   useEffect(() => {
     if (working) {
       document.body.classList.add('working');
+    }
+    if (resting) {
+      document.body.classList.remove('working');
     }
   }, [working]);
 
@@ -39,8 +56,9 @@ export function PomodoroTimer(props: Props) {
 
       <div className="controls">
         <Button text="Work" onClick={() => configureWork()}></Button>
-        <Button text="teste"></Button>
+        <Button text="Rest" onClick={() => configureRest(false)}></Button>
         <Button
+          className={!working && !resting ? 'hidden' : ''}
           text={timeCounting ? 'Pause' : 'Play'}
           onClick={() => setTimeCounting(!timeCounting)}
         ></Button>
